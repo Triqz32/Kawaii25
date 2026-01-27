d9c9b6ac249ab8250aae8ab6b9222e5803275ec9
Triqz32 <elias.larsson@elev.ga.lbs.se>
2026-01-27 15:05:48 +0100

    kod för AI
    Jag säger till vart och när du kan börja lägga blir klar med Ui först innan vi gör resten

--- Assets/Pluggish Ai.cs
+++ Assets/Pluggish Ai.cs
@@ -1,7 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class PluggishAi : MonoBehaviour
 {
+    [SerializeField] TMP_Text chatlog;
+    [SerializeField] TMP_InputField input;
+
+    string content = "Pluggish Ai";// le text som kommer upp via yk
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,25 @@ public class PluggishAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        chatlog.text = content;// det som gör string content funka
     }
+
+    public void Send()
+    {
+        content += "\nDu: " + input.text;
+        Responce(input.text);
+        input.text = "";
+    }
+    
+    void Responce(string msg)
+    {
+        msg= msg.ToLower();
+        string svar = "";
+
+
+        content += "\nPluggish AI: " + svar;
+    }
+    
+    
 }
+
