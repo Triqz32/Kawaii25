1f9114981735531a57c629ce9a086312d0ebfd4f
Triqz32 <elias.larsson@elev.ga.lbs.se>
2026-02-03 12:50:29 +0100

    La till kommentar

--- Assets/Pluggish Ai.cs
+++ Assets/Pluggish Ai.cs
@@ -6,7 +6,7 @@ public class PluggishAi : MonoBehaviour
     [SerializeField] TMP_Text chatlog;
     [SerializeField] TMP_InputField input;
 
-    string content = "Pluggish Ai";// le text som kommer upp via yk
+    string content = "Pluggish Ai";// Elias: le text som kommer upp via chat arean
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,19 +16,19 @@ public class PluggishAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        chatlog.text = content;// det som gör string content funka
+        chatlog.text = content;//Elias: det som gör string content funka
     }
 
     public void Send()
     {
-        content += "\nDu: " + input.text;
-        Responce(input.text);
-        input.text = "";
+        content += "\nDu: " + input.text;// Elias: gör en ny rad och lägger till Du: och sedan vad du har skrivit i input fielden.
+        Responce(input.text);// Elias: gör så att messagen går till coden nedan som behöver en message för att den ska funka.
+        input.text = "";//Elias: Tar bord vad du har skrivit i unput Fielden.
     }
     
-    void Responce(string msg)
+    void Responce(string msg)// Elias: behöver en message för att den ska funka
     {
-        msg= msg.ToLower();
+        msg= msg.ToLower();//Elias: gör allt man har skrivit bli till små bokstäver
         string svar = "";
 
 
